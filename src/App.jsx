fda9c579087db957a9393a8e9fd7466ce4f539e3
babaquero07 <brayanalexanderb@gmail.com>
2023-11-24 16:07:12 -0500

    feat: added portfolio pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import { Home, About, Projects, Contact } from "./pages";
 import NavBar from "./components/NavBar";
 
 const App = () => {
@@ -7,10 +9,10 @@ const App = () => {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={"Home"} />
-          <Route path="/about" element={"About"} />
-          <Route path="/projects" element={"projects"} />
-          <Route path="/contact" element={"Contact"} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
