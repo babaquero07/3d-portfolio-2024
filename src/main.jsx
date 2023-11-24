5b44b5ac31a614d376e729e5e7b1dee3de2f3bcd
babaquero07 <brayanalexanderb@gmail.com>
2023-11-24 15:58:39 -0500

    feat: i18n config

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,29 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// i18n imports & configs
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import en from "./locales/en.json";
+import es from "./locales/es.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      global: es,
+    },
+    en: {
+      global: en,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
