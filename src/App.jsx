dffa7f8dd06dbf6b9a82bced59c6ca30b95c5842
babaquero07 <brayanalexanderb@gmail.com>
2023-11-27 16:04:06 -0500

    sytle: full height class added to app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20">
+    <main className="bg-slate-300/20 h-screen">
       <Router>
         <NavBar />
         <Routes>
