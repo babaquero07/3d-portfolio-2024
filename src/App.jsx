a4db520291e3d800ee5b7101a5c022b54f02487f
babaquero07 <brayanalexanderb@gmail.com>
2023-11-27 20:28:41 -0500

    style: remove h-screen class in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20 h-screen">
+    <main className="bg-slate-300/20">
       <Router>
         <NavBar />
         <Routes>
